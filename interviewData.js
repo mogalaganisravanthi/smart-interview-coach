cc0264ad15faebbe752c3935b3d9797d4a62b52e
Sravanthi Goud <sravanthigoudmogalagani@gmail.com>
2026-06-28 13:01:06 +0530

    a

---
+++ interviewData.js
@@ -0,0 +1,62 @@
+const interviewQuestions = [
+
+{
+question:"Tell me about yourself.",
+
+points:[
+"Name",
+"Education",
+"Department",
+"Skills",
+"Project",
+"Career Goal"
+]
+},
+
+{
+question:"Why should we hire you?",
+
+points:[
+"Skills",
+"Strengths",
+"Teamwork",
+"Problem Solving",
+"Confidence"
+]
+},
+
+{
+question:"What are your strengths?",
+
+points:[
+"Technical Skills",
+"Communication",
+"Leadership",
+"Learning Ability"
+]
+},
+
+{
+question:"Describe one project you have completed.",
+
+points:[
+"Project Name",
+"Objective",
+"Technology",
+"Your Role",
+"Outcome"
+]
+},
+
+{
+question:"Where do you see yourself in 5 years?",
+
+points:[
+"Career Goal",
+"Learning",
+"Growth",
+"Contribution"
+]
+}
+
+];
